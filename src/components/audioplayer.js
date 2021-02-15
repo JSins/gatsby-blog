26feac9ff1094164edfa0e2028ced04122b9d001
JSins <nutzlosundso@gmail.com>
2021-02-15 21:52:50 +0100

    singelpost fertig

--- src/components/audioplayer.js
+++ src/components/audioplayer.js
@@ -1,21 +1,27 @@
 import React from "react"
-import { MdPlayCircleOutline } from "react-icons/md"
-import { MdPauseCircleOutline } from "react-icons/md"
+import { MdPlayArrow } from "react-icons/md"
+import { MdPause } from "react-icons/md"
 
 let audiofile
 let position
+let positiontime
 let duration
+let durationtime
 let progressbar
 let percentposition
 let newposition
 let playing = false
 let playbutton
 let pausebutton
+let showtcurrenttime
+let showduration
 
 function getfile() {
   audiofile = document.getElementById("theaudio")
   playbutton = document.getElementById("playbutton")
   pausebutton = document.getElementById("pausebutton")
+  showtcurrenttime = document.getElementById("showcurrenttime")
+  showduration = document.getElementById("showduration")
 }
 
 function audioplay() {
@@ -37,11 +43,15 @@ function audiopause() {
 function getduration() {
   getfile()
   duration = audiofile.duration
+  durationtime = new Date(duration * 1000).toISOString().substr(15, 4)
+  showduration.innerHTML = durationtime
 }
 
 function getcurrentposition() {
   getfile()
   position = audiofile.currentTime
+  positiontime = new Date(position * 1000).toISOString().substr(15, 4)
+  showtcurrenttime.innerHTML = positiontime
 }
 
 function timer() {
@@ -70,12 +80,20 @@ const Audioplayer = props => {
   return (
     <div className="audioplayer">
       <button id="playbutton" onClick={audioplay}>
-        <MdPlayCircleOutline />
+        <MdPlayArrow />
       </button>
       <button id="pausebutton" onClick={audiopause} style={{ display: "none" }}>
-        <MdPauseCircleOutline />
+        <MdPause />
       </button>
-      <input type="range" id="progressbar" onChange={setposition}></input>
+      <div className="playerinfo">
+        <div id="showcurrenttime" className="showtime">
+          0:00
+        </div>
+        <input type="range" id="progressbar" onChange={setposition}></input>
+        <div id="showduration" className="showtime">
+          3:00
+        </div>
+      </div>
       <audio onTimeUpdate={timer} id="theaudio" preload="auto">
         <source src={props.file}></source>
       </audio>
